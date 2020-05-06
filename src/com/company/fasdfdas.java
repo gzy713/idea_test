59adfb6dc7b1191ab98cf410b1f13fe8142a3296
yss <1261493758@qq.com>
2020-05-06 21:48:38 +0800

    添加类e

--- src/com/company/fasdfdas.java
+++ src/com/company/fasdfdas.java
@@ -5,6 +5,17 @@ public class fasdfdas {
 
     @Override
     protected void finalize() throws Throwable {
+        String  str="afsd";
+        System.out.println(str);
+        System.out.println(str);
+        System.out.println(str);
+        System.out.println(str);
+        System.out.println(str);
+        System.out.println(str);
+        System.out.println(str);
         super.finalize();
+        super.finalize();
+        super.finalize();super.finalize();super.finalize();
+
     }
 }

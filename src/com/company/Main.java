c6bf17bd75bf9c320bf39e953c230e143e410dd5
yss <1261493758@qq.com>
2020-05-06 21:56:10 +0800

    fsafasfsadfds

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
 //    implements java.io.Serializable, Comparable<String>, CharSequence {
     public static void main(String[] args) {
         StringBuffer  str=new StringBuffer();
-        str.append("hello");
+        str.append("hello"); str.append("hello"); str.append("hello"); str.append("hello");
         str.delete(0,2);
         System.out.println(str);
         System.out.println(Integer.MIN_VALUE);
